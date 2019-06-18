7e9395f23b24ec716e1ab43f8c4e830d6ceb2ab4
Danielle Martain <dmartain@gmail.com>
2019-06-18 16:39:10 -0500

    object-group-bonus-calculator

--- client.js
+++ client.js
@@ -40,4 +40,89 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+let Atticus = { 
+name: 'Atticus',
+employeeNumber: '2405',
+annualSalary: '47000',
+reviewRating: 3
+
+}
+let Jem = {
+    name: 'Jem',
+    employeeNumber: '62347',
+    annualSalary: '63500',
+    reviewRating: 4
+}
+
+let Scout = {
+    name: 'Scout',
+    employeeNumber: '6243',
+    annualSalary: '74750',
+    reviewRating: 5
+}
+
+let Robert = {
+    name: 'Robert',
+    employeeNumber: '26835',
+    annualSalary: '66000',
+    reviewRating: 1
+}
+
+let Mayella = {
+    name: 'Mayella',
+    employeeNumber: '89068',
+    annualSalary: '35000',
+    reviewRating: 1
+}
+
+
+function employeeMoney(employee){
+  if (employee.reviewRating === 5 ){
+    return 'base bonus of 10%';
+  }
+  else if (employee.reviewRating === 4 ){
+    return 'base bonus of 6%';
+  }
+  else if (employee.reviewRating === 3 ){
+    return 'base bonus of 4%';
+  }
+  else {
+    return 'no bonus';
+  }
+}
+console.log(employeeMoney(Atticus));
+console.log(employeeMoney(Jem));
+console.log(employeeMoney(Scout));
+console.log(employeeMoney(Robert));
+console.log(employeeMoney(Mayella));
+
+function employmentLength(employee){
+  if ((employee.employeeNumber).length === 4){
+    return 'additional 5%';
+  }
+  else{
+    return 'no additional bonus';
+  }
+}
+
+console.log(employmentLength(Atticus));
+console.log(employmentLength(Jem));
+console.log(employmentLength(Scout));
+console.log(employmentLength(Robert));
+console.log(employmentLength(Mayella));
+
+function employeeSalary(employee){
+  if( employee.annualSalary > 65000 ){
+    return 'bonus adjusted down 1%';
+  }
+  else {
+    return 'no adjustments needed';
+  } 
+}
+
+console.log(employeeSalary(Atticus));
+console.log(employeeSalary(Jem));
+console.log(employeeSalary(Scout));
+console.log(employeeSalary(Robert));
+console.log(employeeSalary(Mayella));
+
